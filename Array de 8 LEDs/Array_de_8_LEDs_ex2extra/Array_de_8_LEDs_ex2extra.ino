83880809797ab89ee9b1fb5b611186cf99b01277
Mohamed Oughssay <mohamed.oughssay.2065@lacetania.cat>
2021-11-14 21:57:02 +0100

    Update Array_de_8_LEDs_ex2extra.ino

--- Array de 8 LEDs/Array_de_8_LEDs_ex2extra/Array_de_8_LEDs_ex2extra.ino
+++ Array de 8 LEDs/Array_de_8_LEDs_ex2extra/Array_de_8_LEDs_ex2extra.ino
@@ -109,16 +109,6 @@ void loop()
   
   delay(temps);                  // Els led's van a una velocitat de 200ms
   
-  digitalWrite(led0, LOW);     // posar a 0V el pin 5
-  digitalWrite(led1, LOW);     // posar a 5V el pin 6
-  digitalWrite(led2, LOW);     // posar a 5V el pin 7
-  digitalWrite(led3, LOW);     // posar a 0V el pin 8
-  digitalWrite(led4, LOW);     // posar a 0V el pin 9
-  digitalWrite(led5, HIGH);     // posar a 5V el pin 10
-  digitalWrite(led6, HIGH);     // posar a 5V el pin 11
-  digitalWrite(led7, LOW);     // posar a 0V el pin 12
-  
-  delay(temps);                  // Els led's van a una velocitat de 200ms
   digitalWrite(led0, LOW);    // posar a 5V el pin 5
   digitalWrite(led1, LOW);    // posar a 5V el pin 6
   digitalWrite(led2, LOW);    // posar a 0V el pin 7
